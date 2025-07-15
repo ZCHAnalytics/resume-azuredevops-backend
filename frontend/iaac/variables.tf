109ed79c54d9bf2316c570dce5f21ebeb0aa7067
Zulfia <zch.analytics@gmail.com>
2025-07-16 00:15:31 +0100

    Restructured repo to add frontedn folder

---
+++ frontend/iaac/variables.tf
@@ -0,0 +1,24 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "storage_account_name" {
+  description = "Name of the storage account"
+  type        = string
+}
+
+variable "cdn_endpoint_name" {
+  description = "Name of the CDN endpoint"
+  type        = string
+}
+
+variable "frontend_origin_urls" {
+  type        = list(string)
+  description = "List of allowed frontend origins for CORS"
+}
